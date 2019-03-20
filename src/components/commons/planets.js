1f643e5a9aece9b11409662ba170270646c3b9b3
leny <info@flatland.be>
2019-03-20 10:55:28 +0100

    Refactor planets component

--- src/components/commons/planets.js
+++ src/components/commons/planets.js
@@ -6,23 +6,23 @@
  * started at 19/03/2019
  */
 
-import React, {useRef, useEffect} from "react";
+import React, {useRef, useEffect, useState} from "react";
 
 import {PLANETS_COLORS} from "../../core/constants";
 
-// TODO: extract draw & animate from useEffect, call requestframe depending on a state boolean
-
 export default ({
+    animate = true,
     bgColor = PLANETS_COLORS[0][0],
     fgColor = PLANETS_COLORS[0][1],
 }) => {
     const canvas = useRef(null);
+    const planets = useRef([]);
+    const colors = useRef({bg: bgColor, fg: fgColor});
+    const [ctx, setCtx] = useState(null);
+    const [size, setSize] = useState(null);
+    const [ready, setReady] = useState(false);
 
-    useEffect(() => {
-        const w = canvas.current.width;
-        const h = canvas.current.height;
-        const ctx = canvas.current.getContext("2d");
-
+    const generatePlanets = ({w, h}) => {
         const large = w / 3;
         const big = w / 10;
         const mid = w / 20;
@@ -31,10 +31,7 @@ export default ({
         const micro = w / 200;
         const dot = w / 500;
 
-        ctx.fillStyle = bgColor;
-        ctx.fillRect(0, 0, w, h);
-
-        let planets = [
+        let _planets = [
             large,
             big,
             mid,
@@ -67,64 +64,103 @@ export default ({
             };
 
             return {
-                size: Math.random() * (i * 1.2 - i * 0.8) + i * 0.8,
+                radius: Math.random() * (i * 1.2 - i * 0.8) + i * 0.8,
                 origin: point,
                 position: point,
                 direction: Math.random() * Math.PI * 2,
             };
         });
 
-        planets.sort(() => Math.random() - 0.5);
+        _planets.sort(() => Math.random() - 0.5);
 
-        const draw = () =>
-            planets.forEach(({size, position: {x, y}}) => {
-                ctx.beginPath();
-                ctx.fillStyle = size > 5 ? bgColor : fgColor;
-                ctx.strokeStyle = fgColor;
-                ctx.lineWidth = 2;
-                ctx.arc(x, y, size, 0, Math.PI * 2, true);
-                ctx.fill();
-                ctx.stroke();
-                ctx.closePath();
-            });
+        return _planets;
+    };
 
-        const animate = () => {
-            ctx.fillStyle = bgColor;
-            ctx.fillRect(0, 0, w, h);
-            planets = planets.map(({size, origin, position, direction}) => {
+    const draw = () =>
+        planets.current.forEach(({radius, position: {x, y}}) => {
+            ctx.beginPath();
+            ctx.fillStyle = radius > 5 ? colors.current.bg : colors.current.fg;
+            ctx.strokeStyle = colors.current.fg;
+            ctx.lineWidth = 2;
+            ctx.arc(x, y, radius, 0, Math.PI * 2, true);
+            ctx.fill();
+            ctx.stroke();
+            ctx.closePath();
+        });
+
+    const update = () => {
+        if (!animate) {
+            return;
+        }
+        ctx.fillStyle = colors.current.bg;
+        ctx.fillRect(0, 0, size.w, size.h);
+        planets.current = planets.current.map(
+            ({radius, origin, position, direction}) => {
                 const change =
                     Math.sqrt(
                         (position.x - origin.x) ** 2 +
                             (position.y - origin.y) ** 2,
-                    ) > Math.abs(100 - size);
+                    ) > Math.abs(100 - radius);
 
                 return {
-                    size,
+                    radius,
                     origin: change ? position : origin,
                     position: {
                         x:
                             position.x +
                             Math.sin(direction) /
-                                Math.ceil(Math.random() * size * 2) /
+                                Math.ceil(Math.random() * radius * 2) /
                                 2.5,
                         y:
                             position.y +
                             Math.cos(direction) /
-                                Math.ceil(Math.random() * size * 2) /
+                                Math.ceil(Math.random() * radius * 2) /
                                 2.5,
                     },
                     direction: change
                         ? Math.random() * (Math.PI * 2)
                         : direction,
                 };
-            });
-            draw();
-            window.requestAnimationFrame(animate);
-        };
-
+            },
+        );
         draw();
-        animate();
+        window.requestAnimationFrame(update);
+    };
+
+    useEffect(() => {
+        console.warn(`useEffect(ready: ${ready}, animate: ${animate})`);
+        if (!ready) {
+            let w, h, context;
+
+            w = canvas.current.width;
+            h = canvas.current.height;
+            context = canvas.current.getContext("2d");
+
+            setSize({w, h});
+            setCtx(context);
+            colors.current = {bg: bgColor, fg: fgColor};
+            planets.current = generatePlanets({w, h});
+
+            context.fillStyle = bgColor;
+            context.fillRect(0, 0, w, h);
+
+            setReady(true);
+        } else {
+            draw();
+            animate && update();
+        }
+    }, [ready]);
+
+    useEffect(() => {
+        console.warn(
+            `useEffect(ready: ${ready}, bgColor: ${bgColor}, fgColor: ${fgColor})`,
+        );
+        if (ready) {
+            colors.current = {bg: bgColor, fg: fgColor};
+        }
     }, [bgColor, fgColor]);
+
+    console.warn(`Planets(bgColor: ${bgColor}, fgColor: ${fgColor})`);
 
     return <canvas width={"1920"} height={"1080"} ref={canvas} />;
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,22 +6,34 @@
  * started at 19/03/2019
  */
 
-import React from "react";
+import React, {useState} from "react";
 
 import {Helmet} from "react-helmet";
 import "../core/font-awesome";
 import GlobalStyles from "../components/head/global-styles";
 
 import Planets from "../components/commons/planets";
+import {PLANETS_COLORS} from "../core/constants";
 
 const styles = {wrapper: {}};
 
-export default () => (
-    <div css={styles.wrapper}>
-        <Helmet>
-            <title>{"leny/uses"}</title>
-        </Helmet>
-        <GlobalStyles />
-        <Planets />
-    </div>
-);
+export default () => {
+    const [index, setIndex] = useState(0);
+
+    return (
+        <div css={styles.wrapper}>
+            <Helmet>
+                <title>{"leny/uses"}</title>
+            </Helmet>
+            <GlobalStyles />
+            <button type={"button"} onClick={() => setIndex(index ? 0 : 1)}>
+                {"Change"}
+            </button>
+            <Planets
+                animate
+                bgColor={PLANETS_COLORS[index][0]}
+                fgColor={PLANETS_COLORS[index][1]}
+            />
+        </div>
+    );
+};
