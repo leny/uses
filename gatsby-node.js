929eb2ceb546e177877dbe37ff164aa2518d57c4
leny <info@flatland.be>
2019-03-25 16:50:43 +0100

    Add workaround for window mock in SSR

---
+++ gatsby-node.js
@@ -0,0 +1,22 @@
+/* leny/uses
+ *
+ * /gatsby-node.js - Gatsby node configuration file
+ *
+ * coded by leny@flatLand!
+ * started at 25/03/2019
+ */
+
+exports.onCreateWebpackConfig = ({stage, loaders, actions}) => {
+    if (stage === "build-html") {
+        actions.setWebpackConfig({
+            module: {
+                rules: [
+                    {
+                        test: /intersection-observer/,
+                        use: loaders.null(),
+                    },
+                ],
+            },
+        });
+    }
+};
